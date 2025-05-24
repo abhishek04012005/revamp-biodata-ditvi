22d6891f0d1a2ec7404964b03ee702da9734c677
2ajaykapoor <2ajaykapoor@gmail.com>
2025-05-24 16:47:24 +0530

    hindi language

---
+++ src/json/Styles.js
@@ -0,0 +1,15 @@
+const DEFAULT_STYLES = {
+  name: {
+    fontSize: "20px",
+  },
+  headings: {
+    fontSize: "16px",
+  },
+  table: {
+    headerFontSize: "14px",
+    dataFontSize: "12px",
+    rowGap: "8px",
+  },
+};
+
+export default DEFAULT_STYLES
